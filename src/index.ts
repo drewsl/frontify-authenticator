60708219b607d3612886edb0fef8c274ca11fdfc
Henrique Serro <henrique.serro@frontify.com>
2021-09-09 10:21:54 +0200

    Added index.ts source and index.html integration example page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
-export default class FrontifyAuthenticator {
-    constructor() {
-        console.log('lala');
-    }
+import { Popup, PopupConfiguration } from "./Popup";
+import {
+    AuthenticationConfig,
+    BearerToken,
+    computeAuthorizationUrl,
+    pollOauthSession,
+    retrieveAccessToken
+} from "./Oauth";
 
-    sayHi(name:string) {
-        console.log('Hello',name);
-    }
+export async function authorize(configuration: AuthenticationConfig, popupConfiguration?: PopupConfiguration): Promise<BearerToken> {
+    const popUp = createPopUp(popupConfiguration ?? {
+        width: 800,
+        height: 600,
+        top: 50,
+        left: 50,
+        title: "Authorize Frontify"
+    });
+
+    const { authorizationUrl, codeVerifier, sessionId } = await computeAuthorizationUrl(configuration);
+    await awaitUserAuthorization(authorizationUrl, popUp);
+    const authorizationCode = await pollOauthSession(configuration, sessionId);
+    return retrieveAccessToken(configuration, authorizationCode, codeVerifier);
+}
+
+async function awaitUserAuthorization(authorizationUrl: string, popUp: Popup) {
+    popUp.navigateToUrl(authorizationUrl);
+
+    return new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => {
+            popUp.close();
+            reject(new Error('Login timeout!'));
+        }, 5 * 60 * 1000);
+
+        popUp.onSuccess(() => {
+            clearTimeout(timeout);
+            popUp.close();
+            resolve(null);
+        });
+
+        popUp.onCancelled(() => {
+            clearTimeout(timeout);
+            popUp.close();
+            reject(new Error('Login canceled!'));
+        });
+    });
+}
+
+function createPopUp(configuration: PopupConfiguration): Popup {
+    return new Popup(configuration ?? {});
 }
